adf02cc8cb7e64e8f8a3e4f65bdc70273b18ae03
Simple Guruji <dashingak957014@gmail.com>
2023-12-03 02:08:13 +0530

    Written registerUser and loginUser controllers.

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,49 @@
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if ([email, fullName, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = await User.findOne({ email });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with this email already exists");
+  }
+
+  const user = await User.create({ fullName, email, password });
+
+  const createdUser = await User.findById(user._id).select("-password");
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User created Successfully"));
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if ([email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.isPasswordCorrect(password))) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "User logged in Successfully"));
+  } else {
+    throw new ApiError(400, "Invalid Credentials");
+  }
+});
+
+export { registerUser, loginUser };
